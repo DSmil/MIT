37126b914a7eb67a3cc8343865573e05505540ac
Davor <davorsmilkov1@gmail.com>
2023-03-11 07:29:29 +0100

    First commit base code

---
+++ src/utils/data.js
@@ -0,0 +1,74 @@
+import c7 from "../components/img/c7.png";
+import fi2 from "../components/img/fi2.png";
+import c3 from "../components/img/c3.png";
+import fi1 from "../components/img/fi1.png";
+
+export const heroData = [
+	{
+		id: 1,
+		name: "Chicken",
+		decp: "with Fries & Salad",
+		price: "300",
+		imageSrc: c7,
+	},
+	{
+		id: 2,
+		name: "Fish",
+		decp: "Grilled Fish",
+		price: "450",
+		imageSrc: fi2,
+	},
+	{
+		id: 3,
+		name: "Chicken",
+		decp: "mixed kebab Plate",
+		price: "280",
+		imageSrc: c3,
+	},
+	{
+		id: 4,
+		name: "Fish",
+		decp: "Mixed Fish Kebab",
+		price: "300",
+		imageSrc: fi1,
+	},
+];
+
+export const categories = [
+	{
+		id: 1,
+		name: "Chicken",
+		urlParamName: "chicken",
+	},
+	{
+		id: 2,
+		name: "Curry",
+		urlParamName: "curry",
+	},
+	{
+		id: 3,
+		name: "Rice",
+		urlParamName: "rice",
+	},
+	{
+		id: 4,
+		name: "Fish",
+		urlParamName: "fish",
+	},
+	{
+		id: 5,
+		name: "Fruits",
+		urlParamName: "fruits",
+	},
+	{
+		id: 6,
+		name: "Icecreams",
+		urlParamName: "icecreams",
+	},
+
+	{
+		id: 7,
+		name: "Soft Drinks",
+		urlParamName: "drinks",
+	},
+];

0e61b8b66bddc35a97b33a575763fad895a540ad
easybi-ds <davor.smilkov@easy.bi>
2023-04-03 00:18:51 +0200

    feat: implemented new firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,13 @@ import { getApp, getApps, initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 const firebaseConfig = {
-  apiKey: "AIzaSyDH1EBaX6OW4Qe553vmypoTl6EpOVTm7Ts",
-  authDomain: "restaurantapplication-49909.firebaseapp.com",
-  databaseURL: "https://restaurantapplication-49909-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "restaurantapplication-49909",
-  storageBucket: "restaurantapplication-49909.appspot.com",
-  messagingSenderId: "350578779926",
-  appId: "1:350578779926:web:d54d43a61db31cf6e15237"
+  apiKey: "AIzaSyBL_oRLiRV5fLNwnrnuef2OYGya6AvvPvU",
+  authDomain: "mitproject-b7c1e.firebaseapp.com",
+  projectId: "mitproject-b7c1e",
+  storageBucket: "mitproject-b7c1e.appspot.com",
+  messagingSenderId: "1006249487321",
+  appId: "1:1006249487321:web:40251368c4c3e1b89807c8",
+  measurementId: "G-ECVKJBZEYD"
 };
 
   const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)

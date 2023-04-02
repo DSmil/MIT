24304525e3b875af0ed70547ef97a9a7ec6c9ada
Kostas15 <kostasfas1999@gmail.com>
2023-04-02 12:51:31 +0200

    CreateContainer and RowContainer styled

--- src/components/CreateContainer/CreateContainer.jsx
+++ src/components/CreateContainer/CreateContainer.jsx
@@ -1,5 +1,4 @@
 import { React, useState } from 'react'
-import { motion } from "framer-motion"
 import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from "react-icons/md"
 import { categories } from 'utils/data'
 import Loader from 'components/Loader/Loader'
@@ -9,6 +8,7 @@ import { saveItem } from 'utils/firebaseFunctions'
 import { useStateValue } from "context/StateProvider";
 import { getAllFoodItems } from "utils/firebaseFunctions";
 import { actionType } from "context/reducer";
+import {BigContainer, NextContainer, MotionP, ButtonDeleteImage, SaveButton, SaveDiv, PriceInput, SetPriceDiv, CaloriesInput, SetCaloriesDiv, SetCaloriesAndPriceDiv, Option2, ImageDiv, Image, UploadText, InputAfterText, Label, AfterLabelUploadDiv, UploadDiv, TitleDiv, TitleInput, Option, SelectCategory, SelectCategoryDiv} from "./style";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
@@ -140,137 +140,124 @@ const CreateContainer = () => {
 
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
+    <BigContainer>
+      <NextContainer>
         {fields && (
-          <motion.p
+          <MotionP
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${alertStatus === "danger"
-              ? "bg-red-400 text-red-800"
-              : "bg-emerald-400 text-emerald-800"
-              }`}
           >
             {msg}
-          </motion.p>
+          </MotionP>
         )}
 
-        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+        <TitleDiv>
           <MdFastfood className="text-xl text-gray-700" />
-          <input
+          <TitleInput
             type="text"
             required
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Give me a title..."
-            className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
           />
-        </div>
+        </TitleDiv>
 
-        <div className="w-full">
-          <select
+        <SelectCategoryDiv>
+          <SelectCategory
             onChange={(e) => setCategory(e.target.value)}
-            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
           >
-            <option value="other" className="bg-white">
+            <Option value="other">
               Select Category
-            </option>
+            </Option>
             {categories &&
               categories.map((item) => (
-                <option
+                <Option2
                   key={item.id}
-                  className="text-base border-0 outline-none capitalize bg-white text-headingColor"
                   value={item.urlParamName}
                 >
                   {item.name}
-                </option>
+                </Option2>
               ))}
-          </select>
-        </div>
+          </SelectCategory>
+        </SelectCategoryDiv>
 
-        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
+        <UploadDiv>
           {isLoading ? (
             <Loader />
           ) : (
             <>
               {!imageAsset ? (
                 <>
-                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
-                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                  <Label >
+                    <AfterLabelUploadDiv>
                       <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
-                      <p className="text-gray-500 hover:text-gray-700">
+                      <UploadText>
                         Click here to upload
-                      </p>
-                    </div>
-                    <input
+                      </UploadText>
+                    </AfterLabelUploadDiv>
+                    <InputAfterText
                       type="file"
                       name="uploadimage"
                       accept="image/*"
                       onChange={uploadImage}
-                      className="w-0 h-0"
                     />
-                  </label>
+                  </Label>
                 </>
               ) : (
                 <>
-                  <div className="relative h-full">
-                    <img
+                  <ImageDiv>
+                    <Image
                       src={imageAsset}
                       alt="uploaded image"
-                      className="w-full h-full object-cover"
                     />
-                    <button
+                    <ButtonDeleteImage
                       type="button"
-                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md  duration-500 transition-all ease-in-out"
                       onClick={deleteImage}
                     >
                       <MdDelete className="text-white" />
-                    </button>
-                  </div>
+                    </ButtonDeleteImage>
+                  </ImageDiv>
                 </>
               )}
             </>
           )}
-        </div>
+        </UploadDiv>
 
-        <div className="w-full flex flex-col md:flex-row items-center gap-3">
-          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+        <SetCaloriesAndPriceDiv>
+          <SetCaloriesDiv>
             <MdFoodBank className="text-gray-700 text-2xl" />
-            <input
+            <CaloriesInput
               type="text"
               required
               value={calories}
               onChange={(e) => setCalories(e.target.value)}
               placeholder="Calories"
-              className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
             />
-          </div>
+          </SetCaloriesDiv>
 
-          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+          <SetPriceDiv>
             <MdAttachMoney className="text-gray-700 text-2xl" />
-            <input
+            <PriceInput
               type="text"
               required
               value={price}
               onChange={(e) => setPrice(e.target.value)}
               placeholder="Price"
-              className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
             />
-          </div>
-        </div>
+          </SetPriceDiv>
+        </SetCaloriesAndPriceDiv>
 
-        <div className="flex items-center w-full">
-          <button
+        <SaveDiv>
+          <SaveButton
             type="button"
-            className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold"
             onClick={saveDetails}
           >
             Save
-          </button>
-        </div>
-      </div>
-    </div>
+          </SaveButton>
+        </SaveDiv>
+      </NextContainer>
+    </BigContainer>
   );
 };
 
--- src/components/RowContainer/RowContainer.jsx
+++ src/components/RowContainer/RowContainer.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { MdShoppingBasket } from "react-icons/md"
-import { motion } from "framer-motion";
 import NotFound from "assets/img/NotFound.svg"
 import { useStateValue } from 'context/StateProvider';
 import { actionType } from 'context/reducer';
+import {RowCont, GetItems, ItemCont, Motion, Image, CartSymbol, TextNotAvailable, ImageNotAvailable, ContNotAvailable, TextCont, Title, PriceSpan, Price, PriceCont, Calories} from "./style";
 
 function RowContainer({ flag, data, scrollValue }) {
 
@@ -31,48 +31,43 @@ function RowContainer({ flag, data, scrollValue }) {
         addToCart()
     }, [items])
     return (
-        <div ref={rowContainer} className={`w-full my-12 flex items-center gap-3 scroll-smooth ${flag ? "overflow-x-scroll scrollbar-none" : "overflow-x-hidden flex-wrap justify-center"}`}>
+        <RowCont ref={rowContainer}>
             {data && data.length > 0 ? data.map(item => (
-                <div key={item.id} className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px] hover:drop-shadow-lg my-12 backdrop-blur-lg bg-cardOverlay rounded-lg flex flex-col items-center justify-between">
-                    <div className="w-full flex items-center justify-between">
-                        <motion.div
-                            className="w-40 h-40 -mt-8 drop-shadow-2xl"
-                            whileHover={{ scale: 1.2 }}
-                        >
-                            <img
+                <ItemCont key={item.id}>
+                    <GetItems>
+                        <Motion whileHover={{ scale: 1.2 }}>
+                            <Image
                                 src={item?.imageURL}
                                 alt=""
-                                className="w-full h-full object-contain"
                             />
-                        </motion.div>
-                        <motion.div
+                        </Motion>
+                        <CartSymbol
                             whileTap={{ scale: 0.75 }}
-                            className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
                             onClick={() => setitems([...cartItems, item])}
                         >
                             <MdShoppingBasket className="text-white" />
-                        </motion.div>
-                    </div>
+                        </CartSymbol>
+                    </GetItems>
 
-                    <div className="w-full flex flex-col items-end justify-end -mt-8">
-                        <p className="text-textColor font-semibold text-base md:text-lg">
+                    <TextCont>
+                        <Title>
                             {item?.title}
-                        </p>
-                        <p className="mt-1 text-sm text-gray-500">
+                        </Title>
+                        <Calories>
                             {item?.calories} Calories
-                        </p>
-                        <div className="flex items-center gap-8">
-                            <p className="text-lg text-headingColor font-semibold">
-                                {item?.price}<span className="text-sm text-red-500"> den.</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            )) : <div className="w-full flex flex-col items-center justify-center">
-                <img src={NotFound} className="h-340" />
-                <p className="text-xl text-headingColor font-semibold my-2">Items Not Available</p>
-            </div>}
-        </div>
+                        </Calories>
+                        <PriceCont>
+                            <Price>
+                                {item?.price}<PriceSpan > den.</PriceSpan>
+                            </Price>
+                        </PriceCont>
+                    </TextCont>
+                </ItemCont>
+            )) : <ContNotAvailable>
+                    <ImageNotAvailable src={NotFound} />
+                    <TextNotAvailable>Items Not Available</TextNotAvailable>
+                </ContNotAvailable>}
+        </RowCont>
     )
 }
 
